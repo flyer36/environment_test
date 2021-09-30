5db54a0f712c9e68fe2d09657be98c504490f6b1
root <root@localhost>
2021-09-30 05:32:05 +0000

    failing unit tests and integration tests for author, price, and published_date attributes

---
+++ spec/feature/price_integration_spec.rb
@@ -0,0 +1,15 @@
+# location: spec/feature/price_integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Creating a book', type: :feature do
+    scenario 'valid price' do
+        visit new_book_path
+        fill_in 'Title', with: 'harry potter'
+        fill_in 'Author', with: 'jk rowling'
+        fill_in 'Price', with: '4.99'
+        fill_in 'Published date', with: '09-20-2021'
+        click_on 'Create Book'
+        visit books_path
+        expect(page).to have_content('4.99')
+    end
+end
